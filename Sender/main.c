dfdbc4c28c6575ffb2e4d05af6121c2086f8ed77
DevYehia <yehiayes20@gmail.com>
2024-02-16 20:26:59 +0200

    Cleaned a bit of the code

--- Sender/main.c
+++ Sender/main.c
@@ -7,98 +7,38 @@
 uint8 fire[] = "Smoke Detected\n";
 uint8 ultra[] = "Object is Near\n";
 uint8 Ourswitch[] = "Switched On\n"; 
-uint8* itoa(int32,uint8*,int32);
 
+/* FOR ULTRASONIC */
 /* global variables to store and display distance in cm */
 uint32 time; /*stores pulse on time */
 uint32 distance; /* stores measured distance value */
-
-
 uint8 lastedgeIn_flag =0;
 int lastEdge, thisEdge;
 unsigned long i = 0;
 
 uint8 systick_done_flag =0;
-//uint8 counter;
 
 int main() {
-//counter=0;
 
-
-
-  //DIO_Init(PORT_F);
-//DIO_SetupDirection(PORT_F,OUT,PIN1,NO_PR);
-  //DIO_SetupDirection(PORT_F,OUT,PIN3,NO_PR);
-  //DIO_SetupDirection(PORT_F,OUT,PIN3,NO_PR);
-  DIO_Init(PORT_F);
-  DIO_SetupDirection(PORT_F,IN,PIN0,PUR);
-  DIO_SetupDirection(PORT_F,IN,PIN4,PUR);
-  interrupt_init(FALLING_EDGE);
-  Set_Bit(GPIO_PORTF_IM_R, PIN0); //Enable interrupt
-  Set_Bit(GPIO_PORTF_IM_R, PIN4); //Enable interrupt
-
-
-  
-  
-  while(1){
-   
-
-    
-  
-  }
-  
-  
+    /*initialize start and stop buttons*/
+    DIO_Init(PORT_F);
+    DIO_SetupDirection(PORT_F,IN,PIN0,PUR);
+    DIO_SetupDirection(PORT_F,IN,PIN4,PUR);
+    interrupt_init(FALLING_EDGE);
+    Set_Bit(GPIO_PORTF_IM_R, PIN0); //Enable interrupt
+    Set_Bit(GPIO_PORTF_IM_R, PIN4); //Enable interrupt
+    while(1){
+    }
 }
 
-uint8* itoa(int32 num, uint8* str, int32 base) {
-	int32 i = 0;
-	int32 isNegative = 0;
-
-	// Handle 0 explicitly
-	if (num == 0) {
-		str[i++] = '0';
-		str[i] = '\0';
-		return str;
-	}
-
-	// Handle negative numbers
-	if (num < 0 && base == 10) {
-		isNegative = 1;
-		num = -num;
-	}
-
-	// Convert the number to string in reverse order
-	while (num != 0) {
-		int32 rem = num % base;
-		str[i++] = (rem > 9) ? (rem - 10) + 'A' : rem + '0';
-		num = num / base;
-	}
-
-	// If the number is negative, append '-'
-	if (isNegative)
-	str[i++] = '-';
-
-	// Append the null terminator
-	str[i] = '\0';
-
-	// Reverse the string
-	int32 start = 0;
-	int32 end = i - 1;
-	while (start < end) {
-		int8 temp = str[start];
-		str[start] = str[end];
-		str[end] = temp;
-		start++;
-		end--;
-	}
-
-	return str;
-}
+//ISR for fume sensor
 void ADCHandler(void){
-  if(ADC0_SSFIFO3_R > 1000){
-     Clear_Bit(NVIC_EN0_R,17);
-    //DIO_WritePin(PORT_F,LOGIC_HIGH,PIN3);
-    Timer2_start();
+
+  if(ADC0_SSFIFO3_R > 1000){//smoke detected
+
+    Clear_Bit(NVIC_EN0_R,17); //disable the ADC interrupt
+
+    Timer2_start(); //start the one second timer
     for(int i=0;i<100000;i++);
     LASER_Off(PORT_A,PIN3);
     for(int i=0;i<100000;i++);
@@ -106,7 +46,6 @@ void ADCHandler(void){
     for(int i=0;i<100000;i++);
     LASER_Off(PORT_A,PIN3);
     for(int i=0;i<100000;i++);
-    
     LASER_On(PORT_A,PIN3);
     for(int i=0;i<100000;i++);
     LASER_Off(PORT_A,PIN3);
@@ -116,14 +55,12 @@ void ADCHandler(void){
     Timer2_stop();
     bluetooth_Write_String(fire);
     
-    Set_Bit(NVIC_EN0_R,17);
+    Set_Bit(NVIC_EN0_R,17);//enable the ADC interrupt
   }
   else{
   }
   Set_Bit(ADC0_PSSI_R,3);
-  Set_Bit(ADC0_ISC_R, 3);
-  
-  
+  Set_Bit(ADC0_ISC_R, 3); //clear the interrupt flag
 }
 
 void SystickHandler(void){
@@ -152,7 +89,7 @@ void TIMER0_HANDLER(void){
     thisEdge = TIMER0_TAR_R;     /* save the timestamp */
     time=(thisEdge - lastEdge); /* return the time difference */
     distance = (time * 10625)/10000000;
-     if (distance >5)
+     if (distance >5) //an object is near
       {
         DIO_WritePin(PORT_F,LOGIC_HIGH,PIN1);
         DIO_WritePin(PORT_F,LOGIC_LOW,PIN3);
@@ -224,28 +161,24 @@ void SwitchHandler(void){
 void PortF_Handler(void){
   //Start button
   if(Get_Bit(GPIO_PORTF_MIS_R,PIN0)){
-    //Ultrasonic configs
 
+    //Ultrasonic configs
     ultrasonic_Init();
 
     SysTickDisable();
-    SysTickPeriodSet(PERIOD_DELAY_10us,uS_UNIT);
-    //SysTickEnable(WITH_INTERRUPT);
+    SysTickPeriodSet(PERIOD_DELAY_10us,uS_UNIT); //10 micro second
 
     Timer1_init();
-    Timer1_setDelay();
-    //Timer1_enableInterrupt();
-
-    //enableInterruptToProcessor(); 
-    
-    
-      //ultrasonic_Init();
-    //DIO_WritePin(PORT_F,LOGIC_HIGH,PIN3);
+    Timer1_setDelay(); //60 ms 
+  
+    //Li_fi communication Timer
     Timer2_init();
     Timer2_setDelay();
+
     bluetooth_Init();
+  
     smoke_Init();
-    //smoke_Enable_Interrupt();
+
     smoke_Start_Sampling();
     LASER_Init(PORT_A,PIN3);
   
